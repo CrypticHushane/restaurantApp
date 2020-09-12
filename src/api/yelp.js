a830a299b90df3730f0ed3fe9ccc4f7a3536c06d
CrypticHushane <oshanewilliams99@outlook.com>
2020-09-11 22:40:09 -0500

    gitignore update

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+require('dotenv').config();
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer jN5E1Pzqia3vIXCAIov1E64I7TaDaUNpwKrHVtFFBjZPm2yW9DegQQQFIYCbPlp_p3JG2lzGijU9nBJLIF04wbF1DXdFBs4Lkw2W2g2Zs-_ZtQitBDHfIoGDkhgdX3Yx'
+        Authorization: `Bearer ${process.env.BEARER_KEY}`
     }
 });
